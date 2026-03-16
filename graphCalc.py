0fd8e2d72967cf058e92354bcee0398c7a66b466
Chizurum <chizurumchiemela@gmail.com>
2026-03-16 17:22:53 +0100

    add automatic axis scaling and range detection

--- graphCalc.py
+++ graphCalc.py
@@ -5,6 +5,37 @@ import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
+def nice_number(value):
+    exponent = np.floor(np.log10(value))
+    fraction = value / 10**exponent
+
+    if fraction < 1.5:
+        nice_fraction = 1
+    elif fraction < 3:
+        nice_fraction = 2
+    elif fraction < 7:
+        nice_fraction = 5
+    else:
+        nice_fraction = 10
+
+    return nice_fraction * 10**exponent
+
+def calculate_axis_limits(data):
+
+    minimum = np.min(data)
+    maximum = np.max(data)
+
+    range_val = maximum - minimum
+
+    tick_spacing = nice_number(range_val / 8)
+
+    axis_min = np.floor(minimum / tick_spacing) * tick_spacing
+    axis_max = np.ceil(maximum / tick_spacing) * tick_spacing
+
+    return axis_min, axis_max, tick_spacing
+
+
+
 st.title("Scientific Graphing Calculator")
 
 st.write("Enter your experimental data")
@@ -24,6 +55,9 @@ if st.button("Generate Graph"):
 
     # reshape for sklearn
     X = x.reshape(-1,1)
+
+    x_min, x_max, x_tick = calculate_axis_limits(x)
+    y_min, y_max, y_tick = calculate_axis_limits(y)
 
     # Linear regression
     model = LinearRegression()
@@ -55,12 +89,30 @@ if st.button("Generate Graph"):
     ))
 
     fig.update_layout(
-        title="Scientific Graph",
-        xaxis_title=x_label,
-        yaxis_title=y_label
+    title="Scientific Graph",
+    xaxis_title=x_label,
+    yaxis_title=y_label,
+
+    xaxis=dict(
+        range=[x_min, x_max],
+        dtick=x_tick
+    ),
+
+    yaxis=dict(
+        range=[y_min, y_max],
+        dtick=y_tick
     )
+)
 
     st.plotly_chart(fig)
+
+    st.subheader("Axis Information")
+
+    st.write(f"X-axis range: {x_min} to {x_max}")
+    st.write(f"X-axis scale: {x_tick}")
+
+    st.write(f"Y-axis range: {y_min} to {y_max}")
+    st.write(f"Y-axis scale: {y_tick}")
 
     st.subheader("Graph Analysis")
 
